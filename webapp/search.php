92b017533361800b67fb212a99a8565aba8a48a8
Mikhail Lipkovich <lipkovich.mikhail@gmail.com>
2015-12-01 19:59:59 +0300

    Combine sugestions for different movies

--- webapp/search.php
+++ webapp/search.php
@@ -3,6 +3,7 @@ $servername = "";
 $username = "";
 $password = "";
 $db = "";
+$suggests_count = 10;
 
 $conn =  mysql_connect($servername, $username, $password);
 
@@ -18,12 +19,36 @@ $movies_arr = array_map("mysql_real_escape_string", $movies_arr);
 $movies_arr = array_map("normalize_title", $movies_arr);
 $movies = implode(',', $movies_arr);
 
-$sql = mysql_query("SELECT movie_title, similar_movies FROM suggestions WHERE movie_title IN ($movies)");
-echo '<p><b>Movies you will probably like: </b></p>';
+echo "<p><b>Your query:</b> $query</p>";
 
-while ($row = mysql_fetch_array($sql)) {
-  echo '<p>'. $row['similar_movies'] .'</p>';
+$sql = mysql_query("SELECT movie_title, similar_movies FROM suggestions WHERE movie_title IN ($movies)");
+
+if (mysql_num_rows($sql)!=0) {
+  echo "<p><b>Movies you will probably like: </b></p>";
+
+  while ($row = mysql_fetch_array($sql)) {
+    $suggests_arr[$row['movie_title']] = explode(';', $row['similar_movies']);
+  }
+
+  // Select $suggests_count suggestions from suggestions for all movies
+  $suggests_arr = array_slice($suggests_arr, 0, $suggests_count);
+  $suggests_per_movie = floor($suggests_count/count($suggests_arr));
+  $suggests_extra = $suggests_count - $suggests_per_movie*count($suggests_arr);
+
+  foreach ($suggests_arr as $suggests) {
+    $display_count = $suggests_per_movie;
+    if ($suggests_extra > 0) {
+      $display_count += 1;
+      $suggests_extra -= 1;
+    }
+    foreach (array_slice($suggests, 0, $display_count) as $title) {
+      echo '<p>' . $title . '</p>';
+    }
+  }
+} else {
+  echo "<p><b>No results found for your query</b></p>";
 }
+
 
 /**
  * Normalizes movie title for using as a key in database (removes "the", commas, etc).
